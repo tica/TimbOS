6b950f63290f0312b3ae8bc33c9803dc6fb9d8c7
Mianki <michael.kirmse91@gmx.de>
2011-10-21 14:09:07 +0200

    +Added German keyboard layout changed default keyboard layout to german

--- src/kb.cpp
+++ src/kb.cpp
@@ -53,7 +53,6 @@ unsigned char kbdus[128] =
     0,	/* All other keys are undefined */
 };
 
-//uppercase US by Michael
 unsigned char kbdus_upper[128] =
 {
 	0,  27, '!', '@', '#', '$', '%', '^', '&', '*',	/* 9 */
@@ -66,6 +65,87 @@ unsigned char kbdus_upper[128] =
 	'\'', '~',   0,		/* Left shift */
 	'\\', 'Z', 'X', 'C', 'V', 'B', 'N',			/* 49 */
 	'M', '<', '>', '?',   0,					/* Right shift */
+	'*',
+	0,	/* Alt */
+	' ',	/* Space bar */
+	0,	/* Caps lock */
+	0,	/* 59 - F1 key ... > */
+	0,   0,   0,   0,   0,   0,   0,   0,
+	0,	/* < ... F10 */
+	0,	/* 69 - Num lock*/
+	0,	/* Scroll Lock */
+	0,	/* Home key */
+	0,	/* Up Arrow */
+	0,	/* Page Up */
+	'-',
+	0,	/* Left Arrow */
+	0,
+	0,	/* Right Arrow */
+	'+',
+	0,	/* 79 - End key*/
+	0,	/* Down Arrow */
+	0,	/* Page Down */
+	0,	/* Insert Key */
+	0,	/* Delete Key */
+	0,   0,   0,
+	0,	/* F11 Key */
+	0,	/* F12 Key */
+	0,	/* All other keys are undefined */
+};
+
+//German Layout
+unsigned char kbger[128] =
+{
+	0,  27, '1', '2', '3', '4', '5', '6', '7', '8',	/* 9 */
+	'9', '0', 'ß', '´', '\b',	/* Backspace */
+	'\t',			/* Tab */
+	'q', 'w', 'e', 'r',	/* 19 */
+	't', 'z', 'u', 'i', 'o', 'p', 'ü', '+', '\n',		/* Enter key */
+	0,			/* 29   - Control */
+	'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'ö',	/* 39 */
+	'\'', 'ä',   0,		/* Left shift */
+	'\\', 'y', 'x', 'c', 'v', 'b', 'n',			/* 49 */
+	'm', ',', '.', '-',   0,					/* Right shift */
+	'*',
+	0,	/* Alt */
+	' ',	/* Space bar */
+	0,	/* Caps lock */
+	0,	/* 59 - F1 key ... > */
+	0,   0,   0,   0,   0,   0,   0,   0,
+	0,	/* < ... F10 */
+	0,	/* 69 - Num lock*/
+	0,	/* Scroll Lock */
+	0,	/* Home key */
+	0,	/* Up Arrow */
+	0,	/* Page Up */
+	'-',
+	0,	/* Left Arrow */
+	0,
+	0,	/* Right Arrow */
+	'+',
+	0,	/* 79 - End key*/
+	0,	/* Down Arrow */
+	0,	/* Page Down */
+	0,	/* Insert Key */
+	0,	/* Delete Key */
+	0,   0,   0,
+	0,	/* F11 Key */
+	0,	/* F12 Key */
+	0,	/* All other keys are undefined */
+};
+
+unsigned char kbger_upper[128] =
+{
+	0,  27, '!', '"', '§', '$', '%', '&', '/', '(',	/* 9 */
+	')', '=', '?', '`', '\b',	/* Backspace */
+	'\t',			/* Tab */
+	'Q', 'W', 'E', 'R',	/* 19 */
+	'T', 'Z', 'U', 'I', 'O', 'P', 'Ü', '*', '\n',		/* Enter key */
+	0,			/* 29   - Control */
+	'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Ö',	/* 39 */
+	'\'', 'Ä',   0,		/* Left shift */
+	'\\', 'Y', 'X', 'C', 'V', 'B', 'N',			/* 49 */
+	'M', ';', ':', '_',   0,					/* Right shift */
 	'*',
 	0,	/* Alt */
 	' ',	/* Space bar */
@@ -140,11 +220,11 @@ struct cpu_state* keyboard_handler(struct cpu_state *r)
 		{
 			if(!shift)
 			{
-				console.printf( "%c", kbdus[scancode] );
+				console.printf( "%c", kbger[scancode] );
 			}
 			else
 			{
-				console.printf( "%c", kbdus_upper[scancode] );
+				console.printf( "%c", kbger_upper[scancode] );
 			}
 		}
     }
