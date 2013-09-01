6be7ac1faf8595d7c098b2dba29fe218f034bfa5
tim <tim.cassens@gmail.com>
2013-09-01 20:02:39 +0200

    Add interlocked compare exchange 64-bit operation

--- src/interlocked.h
+++ src/interlocked.h
@@ -7,8 +7,9 @@ extern "C"
 {
 #endif
 
-	int interlocked_exchange( int* p, int x );
-	int interlocked_compare_exchange( int* p, int comp, int exch );
+	uint32_t interlocked_exchange( uint32_t* p, uint32_t x );
+	uint32_t interlocked_compare_exchange( uint32_t* p, uint32_t comp, uint32_t exch );
+	uint64_t interlocked_compare_exchange_64( uint64_t* p, uint64_t comp, uint64_t exch );
 
 #ifdef __cplusplus
 }
@@ -16,23 +17,28 @@ extern "C"
 
 namespace interlocked
 {
-	inline int exchange( int* p, int x )
+	inline uint32_t exchange( uint32_t* p, uint32_t x )
 	{
 		return interlocked_exchange( p, x );
 	}
 
-	inline bool compare_exchange( int* p, int comp, int exch )
+	inline bool compare_exchange( uint32_t* p, uint32_t comp, uint32_t exch )
 	{
 		return interlocked_compare_exchange( p, comp, exch ) == comp;
 	}
 
-	inline void enter( int* cs )
+	inline bool compare_exchange( uint64_t* p, uint64_t comp, uint64_t exch )
+	{
+		return interlocked_compare_exchange_64( p, comp, exch ) == comp;
+	}
+
+	inline void enter( uint32_t* cs )
 	{
 		while( exchange( cs, 1 ) > 0 )
 			;
 	}
 
-	inline void leave( int* cs )
+	inline void leave( uint32_t* cs )
 	{
 		exchange( cs, 0 );
 	}
--- src/interlocked_.s
+++ src/interlocked_.s
@@ -1,6 +1,6 @@
 
 [global interlocked_exchange]
-	; int interlocked_exchange( int* p, int x );
+	; uint32_t interlocked_exchange( uint32_t* p, uint32_t x );
 interlocked_exchange:
 	mov ecx, [esp+4]	; ecx = p
 	mov eax, [esp+8]	; eax = x
@@ -9,7 +9,7 @@ interlocked_exchange:
 
 
 [global interlocked_compare_exchange]
-	; int interlocked_compare_exchange( int* p, int comp, int exch );
+	; uint32_t interlocked_compare_exchange( uint32_t* p, uint32_t comp, uint32_t exch );
 	; [esp+4] = p
 	; [esp+8] = comp
 	; [esp+12] = exch
@@ -18,4 +18,34 @@ interlocked_compare_exchange:
 	mov ecx, [esp+12]			; ecx = exch
 	mov edx, [esp+4]			; edx = p
 	lock cmpxchg [edx], ecx
+	ret
+
+[global interlocked_compare_exchange_64]
+	; uint64_t interlocked_compare_exchange_64( uint64_t* p, uint64_t comp, uint64_t exch );
+	; [esp+4] = p = [ebp+8 after push ebp, mov ebp esp]
+	; [esp+8] = comp = [ebp+12 after push ebp, mov ebp esp]
+	; [esp+16] = exch = [ebp+20 after push ebp, mov ebp esp]
+interlocked_compare_exchange_64:
+
+	push ebp
+	mov ebp, esp
+
+	push ebx
+	push edi	
+
+	mov edx, [ebp+16]	;
+	mov eax, [ebp+12]	;	edx:eax = comp
+
+	mov ecx, [ebp+24]	;
+	mov ebx, [ebp+20]	;	ecx:ebx = exch
+
+	mov edi, [ebp+8]	;	edi = p
+
+	lock cmpxchg8b	[edi]
+
+	pop edi
+	pop ebx
+
+	pop ebp
+
 	ret
